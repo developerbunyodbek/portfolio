7458753165fe38c6decd00844f34a78c037fc78e
developerbunyodbek <bunyodbekdeveloper01@gmail.com>
2022-01-01 18:58:49 +0500

    Add cursor effect

--- js/main.js
+++ js/main.js
@@ -1,16 +1,40 @@
-const loader = document.getElementById("preloader");
+const elLoader = document.querySelector("#preloader");
+const elToTop = document.querySelector(".back-to-top");
+const cursor = document.querySelector(".cursor");
+const cursorInner = document.querySelector(".cursor2");
 
+// PRELOADER
 window.addEventListener("load", function () {
-  loader.style.display = "none";
+  elLoader.style.display = "none";
 });
 
 // TO TOP SCROLL
-const toTop = document.querySelector(".back-to-top");
-
 window.addEventListener("scroll", () => {
   if (window.pageYOffset > 100) {
-    toTop.classList.add("active");
+    elToTop.classList.add("active");
   } else {
-    toTop.classList.remove("active");
+    elToTop.classList.remove("active");
   }
+});
+
+// CURSOR
+document.addEventListener("mousemove", function (e) {
+  cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+});
+
+document.addEventListener("mousemove", function (e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursorInner.style.left = x + "px";
+  cursorInner.style.top = y + "px";
+});
+
+document.addEventListener("mousedown", function () {
+  cursor.classList.add("click");
+  cursorInner.classList.add("cursorinnerhover");
+});
+
+document.addEventListener("mouseup", function () {
+  cursor.classList.remove("click");
+  cursorInner.classList.remove("cursorinnerhover");
 });
